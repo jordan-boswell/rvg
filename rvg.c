57506951c6bc13afa401489caf52a0a5e4505c6c
jordan-boswell <83526247+jordan-boswell@users.noreply.github.com>
2021-12-07 20:57:15 -0800

    First Working Version

--- rvg.c
+++ rvg.c
@@ -3,18 +3,208 @@
 #include <stdlib.h>
 
 
-enum rvg_error rvg_exp(int (*rng)(), double *points, int n, double rate)
+#ifndef M_PI
+#define M_PI 3.141592653589
+#endif /* M_PI */
+
+
+enum rvg_error rvg_bernoulli(int (*rng)(), int *points, int len, double p)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        double u = (double) rng() / RAND_MAX;
+        if (u < p)
+            points[i] = 1;
+        else
+            points[i] = 0;
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_binomial(int (*rng)(), int *points, int len, int n, double p)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        int count = 0;
+        int j;
+        for (j = 0; j < n; j++) {
+            double u = (double) rng() / RAND_MAX;
+            if (u < p)
+                count++;
+        }
+        points[i] = count;
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_chi_squared(int (*rng)(), double *points, int len, int df)
 {
     double u;
-    for (int i = 0; i < n; i++) {
-        u = (double) rng() / RAND_MAX;
+    int k = df / 2;
+    double accum;
+    if (df % 2 == 0) {
+        int i;
+        for (i = 0; i < len; i++) {
+            int j;
+            accum = 0;
+            for (j = 0; j < k; j++) {
+                u = (double) rng() / RAND_MAX;
+                accum += -2 * log(u);
+            }
+            points[i] = accum;
+        }
+    } else {
+        int i;
+        for (i = 0; i < len; i++) {
+            int j;
+            accum = 0;
+            for (j = 0; j < k; j++) {
+                u = (double) rng() / RAND_MAX;
+                accum += -2 * log(u);
+            }
+            points[i] = accum + log(rng()) * pow(sin(2 * M_PI * rng()), 2);
+        }
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_exp(int (*rng)(), double *points, int len, double rate)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        double u = (double) rng() / RAND_MAX;
         points[i] = -(1.0 / rate) * log(u);
     }
     return RVG_ERR_OK;
 }
 
 
-enum rvg_error rvg_normal(int (*rng)(), double *points, int n, double mean, double sd)
+enum rvg_error rvg_f(int (*rng)(), double *points, int len, int df1, int df2)
+{
+    double u;
+    int k = df2 / 2;
+    double accum;
+    rvg_chi_squared(rng, points, len, df1);
+    if (df2 % 2 == 0) {
+        int i;
+        for (i = 0; i < len; i++) {
+            int j;
+            accum = 0;
+            for (j = 0; j < k; j++) {
+                u = (double) rng() / RAND_MAX;
+                accum += -2 * log(u);
+            }
+            points[i] *= df2 / (df1 * accum);
+        }
+    } else {
+        int i;
+        for (i = 0; i < len; i++) {
+            int j;
+            accum = 0;
+            for (j = 0; j < k; j++) {
+                u = (double) rng() / RAND_MAX;
+                accum += -2 * log(u);
+            }
+            points[i] *= df2 / (df1 * (accum + log(rng()) * pow(sin(2 * M_PI * rng()), 2)));
+        }
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_gamma(int (*rng)(), double *points, int len, double shape, double rate)
+{
+    double u1;
+    double u2;
+    if (shape < 1) {
+        double b = (exp(1) + shape) / exp(1);
+        int i;
+        for (i = 0; i < len; i++) {
+            while (1) {
+                double y;
+                double w;
+                u1 = (double) rng() / RAND_MAX;
+                w = b * u1;
+                if (w < 1) {
+                    y = pow(w, 1.0 / shape);
+                    u2 = (double) rng() / RAND_MAX;
+                    if (u2 <= exp(-y)) {
+                        points[i] = y / rate;
+                        break;
+                    }
+                } else {
+                    y = -log((b - w) / shape); 
+                    u2 = (double) rng() / RAND_MAX;
+                    if (u2 <= pow(y, shape - 1)) {
+                        points[i] = y / rate;
+                        break;
+                    }
+                }
+            }    
+        }
+    } else {
+        double a = 1.0 / sqrt(2 * shape - 1);
+        double b = shape - log(4);
+        double c = shape + 1.0 / a;
+        double d = 1 + log(4.5);
+        int i;
+        for (i = 0; i < len; i++) {
+            while (1) {
+                double v;
+                double y;
+                double z;
+                double w;
+                u1 = (double) rng() / RAND_MAX;
+                u2 = (double) rng() / RAND_MAX;
+                v = a * log(u1 / (1 - u2));
+                y = shape * exp(v);
+                z = u1 * u1 * u2;
+                w = b + c * v - y;
+                if ( w + d - 4.5 * z >= 0) {
+                    points[i] = y / rate;
+                    break;
+                } else if (w >= log(z)) {
+                    points[i] = y / rate;
+                    break;
+                }
+            }
+        }
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_geometric(int (*rng)(), int *points, int len, double p)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        double u = (double) rng() / RAND_MAX;
+        points[i] = ceil(log(u) / log(1 - p));
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_negative_binomial(int (*rng)(), int *points, int len, int n, double p)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        double sum = 0.0;
+        int j;
+        for (j = 0; j < n; j++) {
+            double u = (double) rng() / RAND_MAX;
+            sum += ceil(log(u) / log(1 - p));
+        }
+        points[i] = sum;
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_normal(int (*rng)(), double *points, int len, double mean, double sd)
 {
     const double c0 = 2.515517;
     const double c1 = 0.802853;
@@ -24,14 +214,14 @@ enum rvg_error rvg_normal(int (*rng)(), double *points, int n, double mean, doub
     const double d3 = 0.001308;
     double t;
     double sign;
-    double u;
     double z;
-    for (int i = 0; i < n; i++) {
-        u = (double) rng() / RAND_MAX
+    int i;
+    for (i = 0; i < len; i++) {
+        double u = (double) rng() / RAND_MAX;
         if (u < 1 - u)
-            t = sqrt(-log(u * u))
+            t = sqrt(-log(u * u));
         else
-            t = sqrt(-log((1 - u) * (1 - u)))
+            t = sqrt(-log((1 - u) * (1 - u)));
         if (u - 0.5 < 0)
             sign = -1;
         else if (u - 0.5 > 0)
@@ -45,11 +235,63 @@ enum rvg_error rvg_normal(int (*rng)(), double *points, int n, double mean, doub
 }
 
 
-enum rvg_error rvg_triangular(int (*rng)(), double *points, double a, double b, double c)
+enum rvg_error rvg_poisson(int (*rng)(), int *points, int len, double rate)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        double a, p, x, u;
+        a = exp(-rate);
+        p = 1;
+        x = -1;
+        while (a <= p) {
+            u = (double) rng() / RAND_MAX;
+            p = p * u;
+            x = x + 1;
+        }
+        points[i] = x;
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_t(int (*rng)(), double *points, int len, int df)
 {
     double u;
-    for (int i = 0; i < n; i++) {
-        u = (double) rng() / RAND_MAX;
+    int k = df / 2;
+    double accum;
+    rvg_normal(rng, points, len, 0, 1);
+    if (df % 2 == 0) {
+        int i;
+        for (i = 0; i < len; i++) {
+            int j;
+            accum = 0;
+            for (j = 0; j < k; j++) {
+                u = (double) rng() / RAND_MAX;
+                accum += -2 * log(u);
+            }
+            points[i] /= sqrt(accum / df);
+        }
+    } else {
+        int i;
+        for (i = 0; i < len; i++) {
+            int j;
+            accum = 0;
+            for (j = 0; j < k; j++) {
+                u = (double) rng() / RAND_MAX;
+                accum += -2 * log(u);
+            }
+            points[i] /= sqrt((accum + log(rng()) * pow(sin(2 * M_PI * rng()), 2)) / df);
+        }
+    }
+    return RVG_ERR_OK;
+}
+
+
+enum rvg_error rvg_triangular(int (*rng)(), double *points, int len, double a, double b, double c)
+{
+    int i;
+    for (i = 0; i < len; i++) {
+        double u = (double) rng() / RAND_MAX;
         if (u <= (c - a) / (b - a))
             points[i] = a + sqrt((b - a) * (c - a) * u);
         else
@@ -59,30 +301,23 @@ enum rvg_error rvg_triangular(int (*rng)(), double *points, double a, double b, 
 }
 
 
-enum rvg_error rvg_unif(int (*rng)(), double *points, int n, int a, int b)
+enum rvg_error rvg_unif(int (*rng)(), double *points, int len, int a, int b)
 {
-    double u;
-    for (int i = 0; i < n; i++) {
-        u = (double) rng() / RAND_MAX;
-        points[i] = a + (b - a) * u);
+    int i;
+    for (i = 0; i < len; i++) {
+        double u = (double) rng() / RAND_MAX;
+        points[i] = a + (b - a) * u;
     }
    return RVG_ERR_OK; 
 }
 
 
-enum rvg_error rvg_weibull(int (*rng)(), double *points, int n, double scale, double shape)
+enum rvg_error rvg_weibull(int (*rng)(), double *points, int len, double scale, double shape)
 {
-    double u;
-    for (int i = 0; i < n; i++) {
-        u = rng() / RAND_MAX;
+    int i;
+    for (i = 0; i < len; i++) {
+        double u = rng() / RAND_MAX;
         points[i] = scale * pow(-log(u), 1 / shape);
     }
     return RVG_ERR_OK;
 }
-
-
-int main(int argc, char* argv[])
-{
-    return 0;
-}
-	
--- rvg.h
+++ rvg.h
@@ -1,15 +1,38 @@
 #ifdef RVG_H
 #define RVG_H
 
-struct rvg_rng {
-    int (*rand)();
-    int max;
-};
 
 enum rvg_error {
     RVG_ERR_OK
 };
 
-enum rvg_error rvg_unif_c(int (*rng)(), double *points, int n, int a, int b);
+enum rvg_error rvg_bernoulli(int (*rng)(), int *points, int len, double p);
+
+enum rvg_error rvg_binomial(int (*rng)(), int *points, int len, int n, double p);
+
+enum rvg_error rvg_chi_squared(int (*rng)(), double *points, int len, int df);
+
+enum rvg_error rvg_exp(int (*rng)(), double *points, int len, double rate);
+
+enum rvg_error rvg_f(int (*rng)(), double *points, int len, int df1, int df2);
+
+enum rvg_error rvg_gamma(int (*rng)(), double *points, int len, double shape, double rate);
+
+enum rvg_error rvg_geometric(int (*rng)(), int *points, int len, double p);
+
+enum rvg_error rvg_negative_binomial(int (*rng)(), int *points, int len, int n, double p);
+
+enum rvg_error rvg_normal(int (*rng)(), double *points, int len, double mean, double sd);
+
+enum rvg_error rvg_poisson(int (*rng)(), int *points, int len, double rate);
+
+enum rvg_error rvg_t(int (*rng)(), double *points, int len, int df);
+
+enum rvg_error rvg_triangular(int (*rng)(), double *points, int len, double a, double b, double c);
+
+enum rvg_error rvg_unif(int (*rng)(), double *points, int len, int a, int b);
+
+enum rvg_error rvg_weibull(int (*rng)(), double *points, int len, double scale, double shape);
+
 
 #endif /* RVG_H */
